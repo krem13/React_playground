c0cf2a2059ec2dc6dd05560ec1c2c9354e6f0597
Kremena Dimitrova <krem02@yahoo.com>
2025-08-13 19:10:43 +0200

    fix: border and margins/paddings in track card

--- src/components/TruckCard.tsx
+++ src/components/TruckCard.tsx
@@ -4,9 +4,9 @@ import { TruckCardProps } from "../types/truck.interface";
 export default function TruckCard(props: TruckCardProps) {
   return (
     <Link to={props.page}>
-      <div className="!mb-10 max-w-[400px] mx-5 my-auto p-2.5 rounded-md border-2 border-gray-400 bg-zinc-950 hover:bg-zinc-900 transition-colors">
-        <div className="flex items-center justify-between">
-          <div className="">
+      <div className="!mb-10 max-w-[400px] rounded-md border-2 border-gray-400 bg-zinc-950 hover:bg-zinc-900 transition-colors">
+        <div className="flex items-center justify-between mx-1 my-auto p-2.5">
+          <div>
             <h1 className="text-left text-lg text-white !font-[700]">
               {props.name}
             </h1>
@@ -16,38 +16,40 @@ export default function TruckCard(props: TruckCardProps) {
             From <span className="text-red-500">{props.startPrice}</span>
           </p>
         </div>
-        <div className="flex max-w-[360px] py-4 m-0">
+        <div className="flex max-w-[360px] mt-4">
           <img
             alt="Truck from Marcelis"
             className="w-full h-auto min-h-48 max-h-48 object-cover"
             src={props.pictureUrl}
           />
         </div>
-        <div className="!mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between">
-          <p>Energy source</p>
-          <p className="p-1 rounded-sm bg-[#465e73]">
-            {props.specifications.energySource}
-          </p>
-        </div>
-        <div className="!mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between">
-          <p>GVWR</p>
-          <p className="p-1 rounded-sm bg-[#885c54]">
-            {props.specifications.gvwrMin} - {props.specifications.gvwrMax} kg
-          </p>
-        </div>
-        <div className="!mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between">
-          <p>Payload</p>
-          <p className="p-1 rounded-sm bg-[#6d6748]">
-            {props.specifications.payloadMin} -{" "}
-            {props.specifications.payloadMax} kg
-          </p>
-        </div>
-        <div className="bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between">
-          <p>Capacity</p>
-          <p className="p-1 rounded-sm bg-[#595294]">
-            {props.specifications.capacityMin} -{" "}
-            {props.specifications.capacityMax} m3
-          </p>
+        <div className="my-auto px-2.5 py-4 border-t border-gray-400">
+          <div className="mx-1 !mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between rounded-md">
+            <p>Energy source</p>
+            <p className="p-1 rounded-sm bg-[#465e73]">
+              {props.specifications.energySource}
+            </p>
+          </div>
+          <div className="mx-1 !mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between rounded-md">
+            <p>GVWR</p>
+            <p className="p-1 rounded-sm bg-[#885c54]">
+              {props.specifications.gvwrMin} - {props.specifications.gvwrMax} kg
+            </p>
+          </div>
+          <div className="mx-1 !mb-2 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between rounded-md">
+            <p>Payload</p>
+            <p className="p-1 rounded-sm bg-[#6d6748]">
+              {props.specifications.payloadMin} -{" "}
+              {props.specifications.payloadMax} kg
+            </p>
+          </div>
+          <div className="mx-1 bg-[#2c2c2c] p-2.5 text-[#c2c2c2] flex align-middle justify-between rounded-md">
+            <p>Capacity</p>
+            <p className="p-1 rounded-sm bg-[#595294]">
+              {props.specifications.capacityMin} -{" "}
+              {props.specifications.capacityMax} m3
+            </p>
+          </div>
         </div>
       </div>
     </Link>

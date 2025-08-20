0cf4daed47ea71b72f7ec7f6fb580ee2135b80a9
Kremena Dimitrova <krem02@yahoo.com>
2025-08-20 22:51:24 +0200

    fix: add html structure

--- src/pages/GoogleMapsPage.tsx
+++ src/pages/GoogleMapsPage.tsx
@@ -12,6 +12,24 @@ export default function GoogleMapsPage() {
         <h2 className="text-2xl font-normal my-16 text-gray-900">
           Under construction
         </h2>
+        <div className="flex w-full h-12 px-5 bg-gray-800 text-white justify-between items-center">
+          BOLIGFILTER
+        </div>
+
+        <div className="flex w-full justify-end items-center border border-gray-500">
+          <div className="w-1/5 border-r border-gray-500 p-4 text-gray-500">
+            <h2 className="font-semibold my-2">Ejendomstype:</h2>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Villa</button>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Andelsbolig</button>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Ejerlejlighed</button>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Villalejlighed</button>
+            <h2 className="font-semibold my-2">Byområde:</h2>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Valby</button>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Amager1</button>
+            <button className="border border-gray-500 px-4 py-1 w-full uppercase mb-2">Amager2</button>
+          </div>
+          <div className="w-4/5">Map here</div>
+        </div>
       </div>
     </>
   );

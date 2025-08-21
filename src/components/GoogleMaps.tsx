c9d273480572f82e4c283ab8a7b07c1c14a59777
Kremena Dimitrova <krem02@yahoo.com>
2025-08-21 18:02:38 +0200

    feat: add google map to component

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -1,3 +1,19 @@
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+
 export default function GoogleMaps() {
-  return <p>Map here</p>;
+  const apiKey = process.env.PUBLIC_LOCAL_GOOGLE_MAPS_API_KEY;
+
+  return (
+    <APIProvider apiKey={apiKey ?? ""}>
+      <Map
+        mapId="demo-map"
+        style={{ width: "100%", height: "400px" }}
+        defaultCenter={{ lat: 55.6761, lng: 12.5683 }}
+        defaultZoom={10}
+      >
+        {/* Example marker */}
+        <Marker position={{ lat: 55.6761, lng: 12.5683 }} />
+      </Map>
+    </APIProvider>
+  );
 }
